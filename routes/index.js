b38f27eead6fa28d475aaf81b09a55421a96b59f
mutasem <mutasem.halawani@gmail.com>
2018-05-06 15:06:38 +0300

    remove logging

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,14 @@ router.get('/', (req, res) => {
 router.get('/latest', (req, res) => {
   History.find({}, 'term when -_id').sort('-when')
     .limit(10).then(results => {
+      console.log(results)
       res.json(results)
     })
 })
 
 router.get('/search/:query', (req, res) => {
-  console.log(imgur.getImage())
   imgur.getImage(req.params.query, req.query.offset).then(ans => {
     new History({term: req.params.query}).save()
-    // console.log('ans: ', ans);
     res.json(ans)
   })
 })
